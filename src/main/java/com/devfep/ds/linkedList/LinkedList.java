de4e65fa6d0b704fb4940b1c50226b171dd9a7d9
Felix P <devfep@gmail.com>
2022-11-21 23:59:48 -0500

    LinkedList implementation init

---
+++ src/main/java/com/devfep/ds/linkedList/LinkedList.java
@@ -0,0 +1,20 @@
+package com.devfep.ds.linkedList;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+
+    private Node head;
+    private Node tail;
+
+    
+
+
+}
